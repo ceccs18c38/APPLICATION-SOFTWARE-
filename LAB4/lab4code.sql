7d58ef6cbf58e16b9d3f240b72589f25d6cf9f58
ceccs18c38 <60205860+ceccs18c38@users.noreply.github.com>
2020-11-27 15:21:31 +0530

    Update lab4code.sql

--- LAB4/lab4code.sql
+++ LAB4/lab4code.sql
@@ -1,4 +1,24 @@
+/******************************************************************************
+Margret Jaison 
+S5C
+CHN18CS080
+ROLL NO.: 38
+#Question 
+Create and insert values for the following tables department and instructor(with constraints)
+a.	Department table
+Code(primary key)
+Title 
+Dept_name(unique)
+Dept_id
+Salary(check –salary>2000)
+b.	Instructor table
+Name(not null)
+Code
+Id(default)
+*******************************************************************************/
+
 use lab;
+#a
 CREATE TABLE Department(
     `code` INT PRIMARY KEY NOT NULL,
     `title` VARCHAR(45),
@@ -9,7 +29,7 @@ CREATE TABLE Department(
 
 INSERT INTO Department(`code`, `title`, `dept_name`, `dept_id`,`salary`) VALUES (100, "IQ", "MATHS", 430, 100000);
 SELECT * FROM department;
-
+#b
 CREATE TABLE Instructor(
     `name` VARCHAR(60) NOT NULL,
     `code` INT NOT NULL,
