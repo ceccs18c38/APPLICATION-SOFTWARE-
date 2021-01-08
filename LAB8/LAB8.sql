206cf8c908218c3edebc33f74d39a164f1047a6f
ceccs18c38 <60205860+ceccs18c38@users.noreply.github.com>
2021-01-08 17:09:23 +0530

    Update LAB8.sql

--- LAB8/LAB8.sql
+++ LAB8/LAB8.sql
@@ -1,3 +1,24 @@
+/******************************************************************************
+Margret Jaison 
+S5C
+CHN18CS080
+ROLL NO.: 38
+#Question 
+ 
+ Expt8::Implementation of various aggregate functions in SQL
+Questions:
+
+1.Create a table Employee with following specifications.
+  Code, name, DOB, designation, salary
+2. Insert values into the table
+3. Display sum of salaries from the table employee where designation is clerk.
+4. Describe the maximum salary value from the table employee.
+5.Display average salary of employees
+6. Describe the minimum salary value from the table
+7. Display the total number of employees.
+********************************************************************************/
+
+
 use lab;
 #1 
 CREATE TABLE Employee(code CHAR(5) PRIMARY KEY,
