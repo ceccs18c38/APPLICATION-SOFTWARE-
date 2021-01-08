b131861aa63f7244da4b295e3095cd00e68e48ad
ceccs18c38 <60205860+ceccs18c38@users.noreply.github.com>
2021-01-08 19:01:22 +0530

    Add files via upload

---
+++ LAB9/LAB9.sql
@@ -0,0 +1,25 @@
+use lab9;
+#1
+CREATE TABLE Employee(code CHAR(5) PRIMARY KEY,name VARCHAR(70),dob DATE,designation VARCHAR(80),salary FLOAT
+);
+INSERT INTO Employee(code, name, dob, designation, salary)
+VALUES
+('m1','fiona','1975-05-09','manager',2000000.00),
+('d1','simi','1980-04-12','doctor',1000000.00),
+('n1','diya','1985-08-02','nurse',50000.50);
+#2
+SELECT * FROM Employee ORDER BY name DESC;
+#3
+CREATE TABLE Deposit(
+    baccno BIGINT,
+    branch_name VARCHAR(90),
+    amount FLOAT
+);
+INSERT INTO Deposit(baccno, branch_name, amount)
+VALUES
+(2000000,'madapally',3000000.56),
+(2000001,'thrickodithanam',400000.48),
+(2000002,'changanacherry',5000000.14);
+
+#4
+SELECT branch_name,COUNT(baccno),SUM(amount) FROM Deposit GROUP BY branch_name;
